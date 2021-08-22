5a3759d7cdbb255042fd28ab7e766adfa93c49bc
Timothy White <timothy.white@eagles.oc.edu>
2021-08-22 17:58:17 -0500

    Make dropdown activate on hover instead of click

--- dropdown-menu.js
+++ dropdown-menu.js
@@ -2,7 +2,16 @@ var dropdown = document.getElementsByClassName("dropdown-btn");
 var i;
 
 for (i = 0; i < dropdown.length; i++) {
-  dropdown[i].addEventListener("click", function() {
+  dropdown[i].addEventListener("mouseenter", function() {
+    this.classList.toggle("active");
+    var dropdownContent = this.nextElementSibling;
+    if (dropdownContent.style.display === "block") {
+      dropdownContent.style.display = "none";
+    } else {
+      dropdownContent.style.display = "block";
+    }
+  });
+  dropdown[i].addEventListener("mouseleave", function() {
     this.classList.toggle("active");
     var dropdownContent = this.nextElementSibling;
     if (dropdownContent.style.display === "block") {
