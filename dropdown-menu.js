aab3ae6a9e8f9abf09c673f001bb8a09816d80c6
Timothy White <timothy.white@eagles.oc.edu>
2021-08-22 18:14:48 -0500

    Fixed a bug in the dropdown code

--- dropdown-menu.js
+++ dropdown-menu.js
@@ -11,13 +11,8 @@ for (i = 0; i < dropdown.length; i++) {
       dropdownContent.style.display = "block";
     }
   });
-  dropdown[i].addEventListener("mouseleave", function() {
-    this.classList.toggle("active");
-    var dropdownContent = this.nextElementSibling;
-    if (dropdownContent.style.display === "block") {
-      dropdownContent.style.display = "none";
-    } else {
-      dropdownContent.style.display = "block";
-    }
+  dropdown[i].nextElementSibling.addEventListener("mouseleave", function() {
+    this.previousElementSibling.classList.toggle("active");
+    this.style.display = "none";
   });
 }
